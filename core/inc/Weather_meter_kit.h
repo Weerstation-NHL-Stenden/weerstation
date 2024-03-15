27f1d6245c924f3cdcd49c415f9474ee0a13b1aa
JesperSchuurman <jesper.schuurman@student.nhlstenden.com>
2024-03-15 08:57:24 +0100

    removed the log files and added the straight up code

---
+++ core/inc/Weather_meter_kit.h
@@ -0,0 +1,22 @@
+/*
+ * Weather_meter_kit.h
+ *
+ *  Created on: Jan 12, 2024
+ *      Author: jesper
+ */
+
+#ifndef INC_WEATHER_METER_KIT_H_
+#define INC_WEATHER_METER_KIT_H_
+
+#define WMK_ADC_ANGLE_N 3950
+#define WMK_ADC_ANGLE_NE 3800
+#define WMK_ADC_ANGLE_E 3680
+#define WMK_ADC_ANGLE_SE 3710
+#define WMK_ADC_ANGLE_S 3740
+#define WMK_ADC_ANGLE_SW 3870
+#define WMK_ADC_ANGLE_W 4020
+#define WMK_ADC_ANGLE_NW 4000
+
+extern int windDirectionResitances[8];
+
+#endif /* INC_WEATHER_METER_KIT_H_ */
